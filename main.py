78fe4d54debbb606ce25bc5c0bbd93c44d859fe1
조병화 <chobyounghwa@jobyeonghwaui-MacBookAir.local>
2022-05-13 16:19:01 +0900

    4

--- main.py
+++ main.py
@@ -206,22 +206,21 @@ class Animals:
         # 만약 이동하려햇는데 주변이 다 차있는 경우에는 제자리 이동
         self.move(0, 0, 4)
 
-
     def make_child(self, Lists):
         # 일정 칼로리이상이면 번식한다.
         # 움직이고나서 실행된다
 
-        for i in range(1, self.site+1):
-            k = random.randint(0, len(Site_list_random[i]) -1)
+        for i in range(1, self.site + 1):
+            k = random.randint(0, len(Site_list_random[i]) - 1)
             for j in range(0, len(Site_list_random[i])):
-                child_x = self.x+Site_list_random[i][k - j][0]
-                child_y = self.y+Site_list_random[i][k - j][1]
-                if(child_x>= Grid_size) :
+                child_x = self.x + Site_list_random[i][k - j][0]
+                child_y = self.y + Site_list_random[i][k - j][1]
+                if (child_x >= Grid_size):
                     child_x -= Grid_size
                 if (child_y >= Grid_size):
                     child_y -= Grid_size
 
-                if(Grid[child_x][child_y]==0):
+                if (Grid[child_x][child_y] == 0):
                     a = Animals(child_x, child_y, self.energy_left / 2)
                     Lists.append(a)
                     self.energy_left /= 2
